70e6f2edef49023da0a7fb57904daece400d675b
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-01-18 16:36:26 -0300

    estudando Express

---
+++ backEnd/hello-express/index.js
@@ -0,0 +1,55 @@
+/* index.js */
+const express = require('express');
+const app = express();
+
+const cors = require('cors');
+
+app.use(cors());
+
+const drinks = [
+  { id: 1, name: 'Refrigerante Lata', price: 5.0 },
+  { id: 2, name: 'Refrigerante 600ml', price: 8.0 },
+  { id: 3, name: 'Suco 300ml', price: 4.0 },
+  { id: 4, name: 'Suco 1l', price: 10.0 },
+  { id: 5, name: 'Cerveja Lata', price: 4.5 },
+  { id: 6, name: 'Água Mineral 500 ml', price: 5.0 },
+];
+
+const recipes = [
+  { id: 1, name: 'Lasanha', price: 40.0, waitTime: 30 },
+  { id: 2, name: 'Macarrão a Bolonhesa', price: 35.0, waitTime: 25 },
+  { id: 3, name: 'Macarrão com molho branco', price: 35.0, waitTime: 25 },
+];
+
+app.get('/drinks', function(req, res){
+  const sortedDrinks = drinks.sort((a, b) => a.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase() 
+  < b.name.normalize('NFD').replace(/[\u0300-\u036f]/g, '').toLowerCase() && -1)
+  console.log(sortedDrinks);
+  res.json(sortedDrinks);
+});
+
+app.get('/recipes', function (req, res) {
+  res.json(recipes);
+});
+
+app.get('/recipes/:id', function (req, res) {
+  const { id } = req.params;
+  const recipe = recipes.find((r) => r.id === parseInt(id));
+
+  if (!recipe) return res.status(404).json({ message: 'Recipe not found!'});
+
+  res.status(200).json(recipe);
+});
+
+app.get('/drinks/:id', function (req, res) {
+  const { id } = req.params;
+  const drink = drinks.find((r) => r.id ===parseInt(id))
+
+  if (!drink) return res.status(404).json({ message: 'Recipe not found!'});
+
+  res.status(200).json(drink);
+});
+
+app.listen(3001, () => {
+  console.log('Aplicação ouvindo na porta 3001');
+});
