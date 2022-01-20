59f5629546e2ad5176a34c3794ab81a8128fc792
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-01-20 13:50:04 -0300

    middlewares

--- backEnd/hello-express/index.js
+++ backEnd/hello-express/index.js
@@ -96,6 +96,32 @@ app.delete('/recipes/:id', function (req, res) {
   res.status(204).end();
 });
 
+function validateName(req, res, next) {
+  const { name } = req.body;
+  if (!name || name === '') return res.status(400).json({ message: 'Invalid data!'});
+
+  next();
+};
+
+app.post('/recipes', validateName, function (req, res) {
+  const { id, name, price } = req.body;
+  recipes.push({ id, name, price});
+  res.status(201).json({ message: 'Recipe created successfully!'});
+});
+
+app.put('/recipes/:id', validateName, function (req, res) {
+  const { id } = req.params;
+  const { name, price } = req.body;
+  const recipesIndex = recipes.findIndex((r) => r.id === parseInt(id));
+
+  if (recipesIndex === -1)
+    return res.status(404).json({ message: 'Recipe not found!' });
+
+  recipes[recipesIndex] = { ...recipes[recipesIndex], name, price };
+
+  res.status(204).end();
+});
+
 app.listen(3001, () => {
   console.log('Aplicação ouvindo na porta 3001');
 });
