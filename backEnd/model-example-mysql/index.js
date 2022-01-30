40bb112ab7ef729913546c9d19556fd7059e825c
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-01-30 13:10:34 -0300

    implementa criacao de author

--- backEnd/model-example-mysql/index.js
+++ backEnd/model-example-mysql/index.js
@@ -1,8 +1,11 @@
+const bodyParser = require('body-parser');
 const express = require("express");
 const app = express();
 const port = 3000;
 
 const author = require("./models/author");
+
+app.use(bodyParser.json());
 
 app.get('/authors', async (_req, res) => {
   const authors = await author.getAllAuthors();
@@ -18,6 +21,18 @@ app.get('/authors/:id', async (req, res) => {
   if (!authorById) return res.status(404).json({ message: 'author not found' });
 
   res.status(200).json(authorById);
-})
+});
+
+app.post('/authors', async (req, res) =>{
+  const {first_name, middle_name, last_name} = req.body;
+
+  if(!author.isValid(first_name, middle_name, last_name)) return res.status(400).json(
+    { message: 'invalid data' }
+  );
+
+  await author.createAuthor(first_name, middle_name, last_name);
+
+  res.status(201).json({ message: 'successfully created'});
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- backEnd/model-example-mysql/models/author.js
+++ backEnd/model-example-mysql/models/author.js
@@ -38,7 +38,21 @@ const getAuthorById = async (id) => {
   })
 }
 
+const isValid = (firstName, middleName, lastName) => {
+  if (!firstName || typeof firstName !== 'string') return false;
+  if (!lastName || typeof lastName !== 'string') return false;
+
+  return true;
+}
+
+const createAuthor = async (firstName, middleName, lastName) => connection.execute(
+  'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?, ?, ?)',
+  [firstName, middleName, lastName]
+  ); 
+
 module.exports = {
   getAllAuthors,
-  getAuthorById
+  getAuthorById,
+  isValid,
+  createAuthor,
 }
