fa7f466ee0fa8ff1c75ce7ba563b70247670e0dd
Gabriel Sampaio Bicalho <gabrielsampaiobicalho@gmail.com>
2022-02-15 14:49:38 -0300

    organiza pastas e resolve exercicios de ORM

--- backEnd/hello-msc/models/Author.js
+++ backEnd/hello-msc/models/Author.js
@@ -50,14 +50,6 @@ const findById = async (id) => {
   return serialize(authorData)[0];
 };
 
-const isValid = (firstName, middleName, lastName) => {
-    if (!firstName || typeof firstName !== 'string') return false;
-    if (!lastName || typeof lastName !== 'string') return false;
-    if (middleName && typeof middleName !== 'string') return false;
-
-    return true;
-};
-
 const createAuthor = async (firstName, middleName, lastName) => {
   const [author] = await connection.execute(
     'INSERT INTO model_example.authors (first_name, middle_name, last_name) VALUES (?, ?, ?)',
@@ -69,6 +61,5 @@ const createAuthor = async (firstName, middleName, lastName) => {
 module.exports = {
   getAll,
   findById,
-  isValid,
   createAuthor,
 };
--- backEnd/model-example-mysql/models/author.js
+++ backEnd/model-example-mysql/models/author.js
@@ -38,7 +38,7 @@ const getAuthorById = async (id) => {
   })
 }
 
-const isValid = (firstName, middleName, lastName) => {
+const isValid = (firstName, _middleName, lastName) => {
   if (!firstName || typeof firstName !== 'string') return false;
   if (!lastName || typeof lastName !== 'string') return false;
 
